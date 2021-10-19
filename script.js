777e794e66da4d66fc54764dcbfebe78b1c6ee2b
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2021-10-19 16:33:39 +0530

    Random gradient generator

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-let colorOne = document.getElementById('color-a');
-let colorTwo = document.getElementById('color-b');
-let currentDirection = 'to bottom';
-let outputCode = document.getElementById('code');
+let generateBtn = document.getElementById("generate-btn");
+let copyBtn = document.getElementById("copy-btn");
+let outputColor = document.getElementById("output-color");
+let outputCode = document.getElementById("output-code");
+let hexString = "0123456789abcdef";
 
-function setDirection(value,_this){
-    let directions = document.querySelectorAll(".buttons button");
-    for(let i of directions){
-        i.classList.remove('active');
+let randomColor = () => {
+    let hexCode = "#";
+    for( i=0; i<6; i++){
+        hexCode += hexString[Math.floor(Math.random() * hexString.length)];
     }
-    _this.classList.add('active');
-    currentDirection = value;
+    return hexCode;
 }
 
-function generateCode(){
-    outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value});`
-
-    document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+let generateGrad = () => {
+    let colorOne = randomColor();
+    let colorTwo = randomColor();
+    let angle = Math.floor(Math.random() * 360);
+    outputColor.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
+    outputCode.value = `background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo});`;
 }
 
-function copyText(){
+copyBtn.addEventListener("click", () => {
     outputCode.select();
-    document.execCommand('copy');
-    alert('Gradient Copied!');
-}
+    document.execCommand("copy");
+    alert("Code Copied To Clipboard");
+});
 
-generateCode();
-
+generateBtn.addEventListener("click", generateGrad);
+window.onload = generateGrad;
